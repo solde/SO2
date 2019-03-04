b1239e63f68272d406baab5987b70565367c9184
solde <david.soldevila.puigbi@est.fib.upc.edu>
2019-03-04 19:43:16 +0100

    update

---
+++ lab/entry.s
@@ -0,0 +1,22 @@
+# 1 "entry.S"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "entry.S"
+
+
+
+
+# 1 "include/asm.h" 1
+# 6 "entry.S" 2
+# 1 "include/segment.h" 1
+# 7 "entry.S" 2
+# 72 "entry.S"
+.globl keyboard_handler; .type keyboard_handler, @function; .align 0; keyboard_handler:
+ pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %edx; pushl %ecx; pushl %ebx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es
+ movb $0x20, %al; outb %al, $0x20 ;
+ call keyboard_RSI
+ pop %ebx; pop %ecx; pop %edx; pop %esi; pop %edi; pop %ebp; pop %eax; pop %ds; pop %es; pop %fs; pop %gs;
+ iret
